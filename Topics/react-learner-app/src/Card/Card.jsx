c07d176dd436f85ec57a9fd54c6887a6df945071
shahriar <shahriarkabir078@gmail.com>
2024-05-20 04:26:58 +0600

    new

---
+++ Topics/react-learner-app/src/Card/Card.jsx
@@ -0,0 +1,14 @@
+import styles from './Card.module.css'
+import tasin from '../assets/tasin.png'
+
+function Card(){
+    return(
+        <div className={styles.card}>
+            <img src={tasin} alt="img" />
+            <h3>Shahriar Kabir</h3>
+            <p>Software Engineer @CodeElevate</p>
+            </div>
+    );
+}
+
+export default Card
