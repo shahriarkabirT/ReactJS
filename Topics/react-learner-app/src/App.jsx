383a02bf9eae2520e07c4a018f4183a352c90224
shahriar <shahriarkabir078@gmail.com>
2024-05-22 23:51:14 +0600

    color Picker added

--- Topics/react-learner-app/src/App.jsx
+++ Topics/react-learner-app/src/App.jsx
@@ -11,8 +11,7 @@ function App() {
   return (
     <>
 
-    <Card></Card>
-
+    <ColorPicker></ColorPicker>
     </>
   )
 }
--- Topics/react-learner-app/src/ColorPicker/colorPicker.jsx
+++ Topics/react-learner-app/src/ColorPicker/colorPicker.jsx
@@ -1,5 +1,39 @@
-
+import React , {useState} from 'react';
+import styles from './colorPicker.module.css'
 function colorPicker(){
+    const [colorValue, setColor] = useState("#FFFFFF");
+    const changeColor = (event) =>{
+        setColor(event.target.value);
+    }
 
+
+
+    const copyToClipboard = (event) => {
+        navigator.clipboard.writeText(event.target.value).then(() => {
+          alert('Text copied to clipboard');
+        }).catch(err => {
+          console.error('Failed to copy text: ', err);
+        });
+      };
+
+
+    return(
+        <>
+            
+            <div className={styles.colorPickerContainer}>
+                <div><button className={styles.colorValue} value={colorValue} style={{backgroundColor:colorValue}} onClick={(event)=>copyToClipboard(event) }>
+                    Color value: {colorValue}
+                    </button></div>
+                <div className="textToCopy">
+                <button className={styles.copyButton} value={colorValue} onClick={(event)=>copyToClipboard(event)}>Copy</button>
+                </div>
+
+                <p>Pick a color:</p>
+                <div className={styles.colorPicker}>
+                <input className={styles.input} type="color" value={colorValue} onChange={changeColor} />
+                </div>
+            </div>
+        </>
+    );
 }
 export default colorPicker
