df13432009a6092fd7281576f778997ad8c12e74
shahriar <shahriarkabir078@gmail.com>
2024-05-24 14:35:47 +0600

    added to do app

--- Topics/react-learner-app/src/App.jsx
+++ Topics/react-learner-app/src/App.jsx
@@ -1,18 +1,18 @@
 import Card from './Card/Card.jsx'
- import Parants from './Props/Parants.jsx'
- import Students from './Props/Student.jsx'
-  import Greet from './Conditional_Rendaring/Parant.jsx'
-  import List from './Lists/App.jsx'
-  import ClickEvent from './ClickEvent/App.jsx'
-  import MyComponent from './Hook/UseStateHook/myComponent.jsx'
-  import OnChangeEvent from './OnChangeEvent/onChange.jsx'
-  import ColorPicker from './ColorPicker/colorPicker.jsx'
-  import Hook from './Hook/App.jsx'
-  import ToDoApp from './ToDoApp/ToDoApp.jsx'
+import Parants from './Props/Parants.jsx'
+import Students from './Props/Student.jsx'
+import Greet from './Conditional_Rendaring/Parant.jsx'
+import List from './Lists/App.jsx'
+import ClickEvent from './ClickEvent/App.jsx'
+import OnChangeEvent from './OnChangeEvent/onChange.jsx'
+import ColorPicker from './ColorPicker/colorPicker.jsx'
+import Hook from './Hook/App.jsx'
+import ToDoApp from './ToDoApp/ToDoApp.jsx'
+
 function App() {
   return (
     <>
-    <ToDoApp></ToDoApp>
+  
    
     </>
   )
